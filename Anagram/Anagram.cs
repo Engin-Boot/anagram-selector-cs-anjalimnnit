ae3a7324ad06494d94d5ea306feaf89440106822
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-09-06 13:26:07 -0700

    Passed the test cases

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -6,11 +6,28 @@ namespace Anagram
     public class AnagramSelector
     {
         public bool WordPairIsAnagram(string word1, string word2) {
-            //Insert the correct implementation here
+            if(word1.Length!=word2.Length)
             return false;
+            int[] characterCount = new int[256]; 
+            for(int charInInputString =0;charInInputString<word1.Length;charInInputString++)
+            {
+                characterCount[word1[charInInputString]]++;
+                characterCount[word2[charInInputString]]--;
+            }
+            
+            for(int charInInputString=0;charInInputString<256;charInInputString++)
+            {
+                if(characterCount[charInInputString]!=0)
+                return false;
+            }
+            return true;
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
-            //Insert the correct implementation here
+            for(int i=0;i<candidates.Count;i++)
+            {
+            if(!WordPairIsAnagram(candidates[i],word))
+            candidates.Remove(candidates[i]);
+            }
             return candidates;
         }
     }
