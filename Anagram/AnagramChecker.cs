2e63d98e65ba3243584ddcec3087702c2b8799c3
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-09-06 18:59:54 -0700

    CCN=3

--- Anagram/AnagramChecker.cs
+++ Anagram/AnagramChecker.cs
@@ -4,23 +4,28 @@ using System.Collections.Generic;
 namespace Anagram
 {
     public class AnagramChecker
-    {
-        public bool WordPairIsAnagram(string word1, string word2) {
-
-            int[] characterCount = new int[256]; 
-            for(int charInInputString =0;charInInputString<word1.Length;charInInputString++)
-            {
-                characterCount[word1[charInInputString]]++;
-                characterCount[word2[charInInputString]]--;
-            }
-            
+    {   
+         int[] characterCount = new int[256]; 
+        public bool CheckAllZerosInArray(string word1, string word2)
+        {
             for(int charInInputString=0;charInInputString<256;charInInputString++)
             {
-                if(word1.Length!=word2.Length || characterCount[charInInputString]!=0)
+                if( characterCount[charInInputString]!=0)
                 return false;
             }
-            
             return true;
+        }
+        public bool WordPairIsAnagram(string word1, string word2)
+         {
+             for(int charInInputString =0 ;charInInputString<word1.Length;charInInputString++)
+             {
+                characterCount[word1[charInInputString]]++;
+                characterCount[word2[charInInputString]]--;
+             }
+             if(word1.Length!=word2.Length )
+             return false;
+            return CheckAllZerosInArray(word1,word2);     
+          
         }
     }
 }
