6f78d3279f87bfc5847a85bef431c2542bf98780
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-09-06 19:06:18 -0700

    removed errors

--- Anagram/AnagramChecker.cs
+++ Anagram/AnagramChecker.cs
@@ -6,7 +6,7 @@ namespace Anagram
     public class AnagramChecker
     {   
          int[] characterCount = new int[256]; 
-        public bool CheckAllZerosInArray(string word1, string word2)
+        public bool CheckAllZerosInArray()
         {
             for(int charInInputString=0;charInInputString<256;charInInputString++)
             {
@@ -17,14 +17,14 @@ namespace Anagram
         }
         public bool WordPairIsAnagram(string word1, string word2)
          {
+             if(word1.Length!=word2.Length )
+             return false;
              for(int charInInputString =0 ;charInInputString<word1.Length;charInInputString++)
              {
                 characterCount[word1[charInInputString]]++;
                 characterCount[word2[charInInputString]]--;
              }
-             if(word1.Length!=word2.Length )
-             return false;
-            return CheckAllZerosInArray(word1,word2);     
+            return CheckAllZerosInArray();     
           
         }
     }
